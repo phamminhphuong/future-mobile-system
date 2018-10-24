8af068740e158b0336344c826b8b1c2e1d0573a8
phamminhphuong <phuongninhbinh7696@gmail.com>
2018-10-24 15:16:55 +0700

    add admin layout

--- db/migrate/20181021153842_create_order_dertails.rb
+++ db/migrate/20181021153842_create_order_dertails.rb
@@ -1,4 +1,4 @@
-class CreateOrderDetails < ActiveRecord::Migration[5.2]
+class CreateOrderDertails < ActiveRecord::Migration[5.2]
   def change
     create_table :order_details do |t|
       t.integer :quantity
