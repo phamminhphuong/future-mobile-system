40e3d03f287fe947fee81451f591565a5ae9ed67
buitiendo <bui.tien.do@framgia.com>
2018-10-25 12:57:31 +0700

    add message error

--- db/migrate/20181021075523_create_accounts.rb
+++ db/migrate/20181021075523_create_accounts.rb
@@ -6,7 +6,7 @@ class CreateAccounts < ActiveRecord::Migration[5.2]
       t.string :fullname
       t.string :address
       t.string :phone
-      t.integer :account_type
+      t.integer :account_type, null: false, default: 0
 
       t.timestamps
     end
