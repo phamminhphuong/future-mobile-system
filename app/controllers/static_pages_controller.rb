fe8ca506d2048a7f88de3d859b95b87623555bd2
buitiendo <bui.tien.do@framgia.com>
2018-10-29 17:18:55 +0700

    show data product-detail, layout add to card

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
-  def home; end
+  def home
+    @products = Product.select_products
+      .page(params[:page])
+      .per Settings.size.size_page
+    @details = Product.show_detail(params[:id])
+  end
 
   def about; end
 end
